3adf1c5cef89c7703d322af4d2392f0c9e21dee4
nil <Nilrajsinh6005@ICLOUD.COM>
2019-08-09 22:53:27 +0530

    Final code

--- First_ui/ViewController.swift
+++ First_ui/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
+        
     }
-
-
+    
+    @IBOutlet weak var LBL: UILabel!
+    
+    @IBAction func Btn(_ sender: Any) {
+        LBL.text = "here is the code"
+    }
+    
+    
 }
 
